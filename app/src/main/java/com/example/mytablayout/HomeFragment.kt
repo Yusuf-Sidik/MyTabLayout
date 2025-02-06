522ba3d6760c4a6d8b99133e2c190ed72653deb4
yusuf <yusufnew060@gmail.com>
2025-02-06 10:34:47 +0700

    Mengirim data dari Activity ke Fragment

--- app/src/main/java/com/example/mytablayout/HomeFragment.kt
+++ app/src/main/java/com/example/mytablayout/HomeFragment.kt
@@ -20,10 +20,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val tvLabel: TextView = view.findViewById(R.id.section_tabel)
         val index = arguments?.getInt(ARG_SECTION_NUMBER, 0)
+        val name = arguments?.getString(ARG_NAME)
         tvLabel.text = getString(R.string.content_tab_text, index)
     }
 
     companion object {
         const val ARG_SECTION_NUMBER = "section_number"
+        const val ARG_NAME = "app_name"
     }
 }
--- app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import kotlinx.coroutines.awaitAll
 
 class SectionsPagerAdapter(activity: AppCompatActivity) :
 FragmentStateAdapter (activity) {
+
+    var appName: String = ""
     override fun getItemCount(): Int {
         return 4
     }
@@ -15,6 +18,7 @@ FragmentStateAdapter (activity) {
         val fragment = HomeFragment()
         fragment.arguments = Bundle().apply {
             putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            putString(HomeFragment.ARG_NAME, appName)
         }
         return fragment
     }
